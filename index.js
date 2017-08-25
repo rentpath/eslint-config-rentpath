0eaf815b031c1ff70676eab41350cadc33d9d0d5
Tad Johnston <tjohnston@rentpath.com>
2017-08-25 07:51:39 -0700

    upgrading eslint - enforcing more standards based on current and past PR issues - adding plugins necessary for react and testing work

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
     'eslint-config-airbnb',
     './rules/style',
     './rules/es6',
+    './rules/react',
   ].map(require.resolve),
   rules: {}
 }
--- rules/es6.js
+++ rules/es6.js
@@ -6,5 +6,9 @@ module.exports = {
     'arrow-parens': [2, 'as-needed'],
     // suggest using arrow functions as callbacks
     'prefer-arrow-callback': 2,
+    // allow file with no default export
+    'import/prefer-default-export': 0,
+    // ignore references to root directory
+    'import/no-extraneous-dependencies': 0,
   }
 }
