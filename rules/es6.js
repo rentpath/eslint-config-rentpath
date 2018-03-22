0c7664cdbafc97188960503c264f982b27060486
Luke Fender <lfender6445@gmail.com>
2018-03-22 15:16:27 -0400

    Disable some rules

--- rules/es6.js
+++ rules/es6.js
@@ -1,14 +1,19 @@
 module.exports = {
   rules: {
     // enforces no braces where they can be omitted
-    'arrow-body-style': [2, 'as-needed'],
+    // https://eslint.org/docs/rules/arrow-body-style
+    'arrow-body-style': 0,
     // require parens in arrow function arguments
-    'arrow-parens': [2, 'as-needed'],
+    // https://eslint.org/docs/rules/arrow-parens
+    'arrow-parens': 0,
     // suggest using arrow functions as callbacks
     'prefer-arrow-callback': 2,
     // allow file with no default export
     'import/prefer-default-export': 0,
     // ignore references to root directory
     'import/no-extraneous-dependencies': 0,
+    // allow import { default as Lib } from 'lib'
+    // overrides https://github.com/airbnb/javascript#modules--no-export-from-import
+    "import/no-named-default": 0
   }
 }
